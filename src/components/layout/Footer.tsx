6fd639222b0b8420a54b29f44b18cf61f47e1d94
Mfikryyyyyy11 <mfikryhaikal8@gmail.com>
2025-03-16 09:27:43 +0700

    some change on Hero section and add feature section

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div className="relative w-full min-h-screen bg-red-200">
+      <div className="absolute w-[70%] h-[250px] bg-white shadow-md rounded-xl left-1/2 -translate-x-1/2"></div>
+    </div>
+  );
+}
